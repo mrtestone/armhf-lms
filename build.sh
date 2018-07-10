6cba6fc2ba989802674077616fe37f0560c83407
mrtestone <41049971+mrtestone@users.noreply.github.com>
2018-07-10 23:18:16 +0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -4,9 +4,10 @@
 # lms image tag
 VERSION=7.9.1
 DOCKERHUBUSER=mrtestone
+REPONAME=armhf-lms
 
 # docker image to build
-IMAGE_NAME=$DOCKERHUBUSER/rpi-lms:$VERSION
+IMAGE_NAME=$DOCKERHUBUSER/$REPONAME:$VERSION
 
 # build runtime
 sudo docker build -t $IMAGE_NAME .
